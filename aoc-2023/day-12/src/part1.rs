76fa8120e1b98fd66fbee59244658717f7ff475f
Nguyen Renan <en1.acg@outlook.com>
2023-12-13 10:56:02 +0700

    Solve part 1

--- aoc-2023/day-12/src/part1.rs
+++ aoc-2023/day-12/src/part1.rs
@@ -1,43 +1,86 @@
+use indicatif::ProgressIterator;
+use itertools::{repeat_n, Itertools};
+
 #[derive(Debug)]
 struct DataStruct<'a> {
-    records: Vec<&'a str>,
-    damged_springs: Vec<Vec<u32>>,
+    records: Vec<Record<'a>>,
 }
-impl<'a> DataStruct<'a> {
-    fn solve(&self, row: usize) {
-        let damged = &self.damged_springs[row];
-        let record = self.records[row].chars();
-        loop {}
+#[derive(Debug)]
+struct Record<'a> {
+    line: &'a str,
+    batches: Vec<u32>,
+    unknow_spring: u32,
+}
+
+impl<'a> Record<'a> {
+    fn generate_combination(&self) -> impl Iterator<Item = Vec<char>> {
+        repeat_n(['.', '#'].into_iter(), self.unknow_spring as usize).multi_cartesian_product()
+    }
+    fn check_combination(&self, combination: &Vec<char>) -> bool {
+        let mut combination_iter = combination.into_iter();
+        let fill_record = self
+            .line
+            .chars()
+            .map(|c| match c {
+                '?' => *combination_iter
+                    .next()
+                    .expect("should have enough for replacement"),
+                value => value,
+            })
+            .group_by(|c| c == &'#')
+            .into_iter()
+            .filter_map(|(is_hash_group, group)| {
+                is_hash_group.then_some(group.into_iter().count() as u32)
+            })
+            .collect::<Vec<u32>>();
+        // dbg!(&fill_record);
+        self.batches[..] == fill_record[..]
+    }
+    fn possible_solution_count(&self) -> u32 {
+        self.generate_combination()
+            .filter(|combination| self.check_combination(combination))
+            .count() as u32
     }
 }
 
-struct UnknowSpring {}
+impl<'a> DataStruct<'a> {
+    fn solve(&self) -> String {
+        self.records
+            .iter()
+            .progress()
+            .map(|record| record.possible_solution_count())
+            .sum::<u32>()
+            .to_string()
+    }
+}
 
 fn parse(input: &str) -> DataStruct {
     input.lines().fold(
         DataStruct {
             records: Vec::new(),
-            damged_springs: Vec::new(),
         },
         |mut acc, line| {
             let mut iter = line.split_whitespace();
-            acc.records.push(iter.next().expect("records"));
-            acc.damged_springs.push(
-                iter.next()
+            let data = iter.next().expect("records");
+            let record = Record {
+                line: data,
+                batches: iter
+                    .next()
                     .expect("damged_springs")
                     .split(',')
                     .map(|number| number.parse::<u32>().expect("parse number ok"))
                     .collect::<Vec<u32>>(),
-            );
+                unknow_spring: data.chars().filter(|c| c == &'?').count() as u32,
+            };
+            acc.records.push(record);
             acc
         },
     )
 }
 pub fn process(input: &str) -> String {
-    let result = parse(input);
-    dbg!(result);
-
-    todo!()
+    let data = parse(input);
+    data.solve()
+    // dbg!(result);
 }
 #[cfg(test)]
 mod tests {
--- aoc-2023/day-12/src/part2.rs
+++ aoc-2023/day-12/src/part2.rs
@@ -1,5 +1,89 @@
+use itertools::{repeat_n, Itertools};
+
+#[derive(Debug)]
+struct DataStruct {
+    records: Vec<Record>,
+}
+#[derive(Debug)]
+struct Record {
+    line: String,
+    batches: Vec<u32>,
+    unknow_spring: u32,
+}
+
+impl Record {
+    fn generate_combination(&self) -> impl Iterator<Item = Vec<char>> {
+        repeat_n(['.', '#'].into_iter(), self.unknow_spring as usize).multi_cartesian_product()
+    }
+    fn check_combination(&self, combination: &Vec<char>) -> bool {
+        let mut combination_iter = combination.iter();
+        let fill_record = self
+            .line
+            .chars()
+            .map(|c| match c {
+                '?' => *combination_iter
+                    .next()
+                    .expect("should have enough for replacement"),
+                value => value,
+            })
+            .group_by(|c| c == &'#')
+            .into_iter()
+            .filter_map(|(is_hash_group, group)| {
+                is_hash_group.then_some(group.into_iter().count() as u32)
+            })
+            .collect::<Vec<u32>>();
+        // dbg!(&fill_record);
+        self.batches[..] == fill_record[..]
+    }
+    fn possible_solution_count(&self) -> u32 {
+        self.generate_combination()
+            .filter(|combination| self.check_combination(combination))
+            .count() as u32
+    }
+}
+
+impl DataStruct {
+    fn solve(&self) -> String {
+        self.records
+            .iter()
+            .map(|record| record.possible_solution_count())
+            .sum::<u32>()
+            .to_string()
+    }
+}
+
+fn parse(input: &str) -> DataStruct {
+    input.lines().fold(
+        DataStruct {
+            records: Vec::new(),
+        },
+        |mut acc, line| {
+            let mut iter = line.split_whitespace();
+            let data = iter.next().expect("records");
+            let unknow_spring = data.chars().filter(|c| c == &'?').count() as u32;
+            let unknow_spring = unknow_spring * 5 + 4;
+            let data = std::iter::repeat(data).take(5).join("?");
+            let batches = iter
+                .next()
+                .expect("damged_springs")
+                .split(',')
+                .map(|number| number.parse::<u32>().expect("parse number ok"))
+                .collect::<Vec<u32>>();
+            let batches = std::iter::repeat(batches).take(5).flatten().collect_vec();
+            let record = Record {
+                line: data,
+                batches,
+                unknow_spring,
+            };
+            acc.records.push(record);
+            acc
+        },
+    )
+}
 pub fn process(input: &str) -> String {
-    todo!();
+    let data = parse(input);
+    data.solve()
+    // dbg!(result);
 }
 #[cfg(test)]
 mod tests {
@@ -7,16 +91,19 @@ mod tests {
 
     #[test]
     fn test_process() {
-        let input = "467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..";
-        assert_eq!("4361", process(input))
+        let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+";
+        assert_eq!("525152", process(input))
     }
 }
+
+/*
+(0, 3), (3, 3) | 1, 1, 3
+(0, 2), (0, 2), (2, 3) | 1, 1, 3
+(2, 15) | 1, 3, 1, 6
+ */
